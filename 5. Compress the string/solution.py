bd81f1fc9937b8e4d05f579aae2b4a6ee41a3d99
Avin Divakara <divakaraavin@gmail.com>
2023-07-17 18:52:01 +0530

    5th Question answer

---
+++ 5. Compress the string/solution.py
@@ -0,0 +1,16 @@
+s = input()
+
+count = 0
+user_inputs = [int(s[i]) for i in range(len(s))]
+cfi = 0 # Continue From Index
+number = -1
+
+while number!=user_inputs[cfi]:
+    number = user_inputs[cfi]
+    for i in range (cfi, len(user_inputs)):
+        if user_inputs[cfi] != user_inputs[i]:
+            cfi=i
+            break
+        count += 1 
+    print((count, number), end=" ")
+    count=0
